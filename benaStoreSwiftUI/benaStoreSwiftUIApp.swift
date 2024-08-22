ec4dd5135968752c2fff9ce901e10e38159c1b19
Minata P Mahaarta <minataputra@gmail.com>
2024-08-22 10:58:58 +0700

    Initial Commit

---
+++ benaStoreSwiftUI/benaStoreSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  benaStoreSwiftUIApp.swift
+//  benaStoreSwiftUI
+//
+//  Created by Minata on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct benaStoreSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
